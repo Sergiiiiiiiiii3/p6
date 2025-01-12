7c8ef6dd2a0f726e52c25c2860b578ca24a370af
Sergiiiiiiiiii3 <s3rgi3scarre@gmail.com>
2025-01-12 10:59:47 +0100

    Add files via upload

---
+++ js/about.c2861778.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkp3"]=self["webpackChunkp3"]||[]).push([[594],{1417:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1656),l={},r=(0,a.A)(l,s,e,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.c2861778.js.map
